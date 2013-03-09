5745b8bc655998e024ebe6d2f9b44da621532e28
Yari @yariok <yari@jumpzero.com>
2013-03-09 18:00:52 +0100

    Init Git

---
+++ SpeedGraphics/TestingView.m
@@ -0,0 +1,32 @@
+//
+//  TestingView.m
+//  SpeedGraphics
+//
+//  Created by Yari Dareglia on 3/9/13.
+//  Copyright (c) 2013 Yari Dareglia. All rights reserved.
+//
+
+#import "TestingView.h"
+#import "SpeedGraphics.h"
+
+@implementation TestingView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+
+
+- (void)drawRect:(CGRect)rect
+{
+
+
+}
+
+
+@end
---
+++ SpeedGraphics/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  SpeedGraphics
+//
+//  Created by Yari Dareglia on 3/9/13.
+//  Copyright (c) 2013 Yari Dareglia. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
